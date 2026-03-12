53de28e06aa4b3c55f3ea8b15810187de1bfec55
Mohd Salauddin <sallumalik1111@gmail.com>
2026-03-12 20:57:51 +0530

    feat:Implement the admin repository

---
+++ lib/features/admin/data/repositories/admin_repository_impl.dart
@@ -0,0 +1,36 @@
+import 'package:dartz/dartz.dart';
+import '../../../../core/errors/exceptions.dart';
+import '../../../../core/errors/failures.dart';
+import '../../domain/entities/admin_stats_entity.dart';
+import '../../domain/repositories/admin_repository.dart';
+import '../datasources/admin_remote_datasource.dart';
+
+class AdminRepositoryImpl implements AdminRepository {
+  final AdminRemoteDataSource _remoteDataSource;
+
+  AdminRepositoryImpl(this._remoteDataSource);
+
+  @override
+  Future<Either<Failure, AdminDashboardStatsEntity>> getDashboardStats() async {
+    try {
+      final stats = await _remoteDataSource.getDashboardStats();
+      return Right(stats);
+    } on ServerException catch (e) {
+      return Left(ServerFailure(message: e.message));
+    } catch (e) {
+      return Left(ServerFailure(message: e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> suspendUser(String uid, bool isSuspended) async {
+    try {
+      await _remoteDataSource.suspendUser(uid, isSuspended);
+      return const Right(null);
+    } on ServerException catch (e) {
+      return Left(ServerFailure(message: e.message));
+    } catch (e) {
+      return Left(ServerFailure(message: e.toString()));
+    }
+  }
+}
