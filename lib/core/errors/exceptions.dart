a1fadc0b5a805225cc5ce8588b7a4af7a287eb95
Mohd Salauddin <sallumalik1111@gmail.com>
2026-03-12 20:53:20 +0530

    Feat:fix the exception file

--- lib/core/errors/exceptions.dart
+++ lib/core/errors/exceptions.dart
@@ -1,24 +1,39 @@
 class ServerException implements Exception {
   final String message;
   const ServerException({this.message = 'Server error occurred'});
+
+  @override
+  String toString() => message;
 }
 
 class AuthException implements Exception {
   final String message;
   const AuthException({required this.message});
+
+  @override
+  String toString() => message;
 }
 
 class NetworkException implements Exception {
   final String message;
   const NetworkException({this.message = 'No internet connection'});
+
+  @override
+  String toString() => message;
 }
 
 class CacheException implements Exception {
   final String message;
   const CacheException({required this.message});
+
+  @override
+  String toString() => message;
 }
 
 class NotFoundException implements Exception {
   final String message;
   const NotFoundException({this.message = 'Resource not found'});
+
+  @override
+  String toString() => message;
 }
