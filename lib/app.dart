2243d1578c36fc74952ddf108fa14fc0dc40c716
Mohd Salauddin <sallumalik1111@gmail.com>
2026-03-22 21:58:03 +0530

    fix error in the app file

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'core/theme/app_theme.dart';
+import 'features/auth/presentation/bloc/auth_bloc.dart';
+import 'features/auth/presentation/bloc/auth_event.dart';
+import 'features/auth/presentation/bloc/auth_state.dart';
+import 'features/calls/presentation/cubit/call_cubit.dart';
+import 'features/calls/presentation/cubit/call_state.dart';
+import 'injection/injection.dart';
 import 'navigation/app_router.dart';
+import 'navigation/route_names.dart';
 
 class AlumniConnectApp extends StatelessWidget {
   const AlumniConnectApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Alumni Connect',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.darkTheme,
-      routerConfig: appRouter,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => getIt<AuthBloc>()..add(const AuthCheckRequested()),
+        ),
+        BlocProvider(
+          create: (_) => getIt<CallCubit>(),
+        ),
+      ],
+      child: MultiBlocListener(
+        listeners: [
+          BlocListener<AuthBloc, AuthState>(
+            listener: (context, state) {
+              if (state is AuthUnauthenticated) {
+                appRouter.go(RouteNames.login);
+              } else if (state is AuthAuthenticated) {
+                context.read<CallCubit>().listenForIncomingCalls(state.user.uid);
+              }
+            },
+          ),
+          BlocListener<CallCubit, CallState>(
+            listener: (context, state) {
+              if (state is CallIncoming) {
+                appRouter.push(RouteNames.voiceCall);
+              }
+            },
+          ),
+        ],
+        child: MaterialApp.router(
+          title: 'Alumni Connect',
+          debugShowCheckedModeBanner: false,
+          theme: AppTheme.darkTheme,
+          routerConfig: appRouter,
+        ),
+      ),
     );
   }
 }
