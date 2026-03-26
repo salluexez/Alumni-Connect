a0dd0d59e136227cdc3e168da1304aa805603bbe
Mohd Salauddin <sallumalik1111@gmail.com>
2026-03-26 11:12:30 +0530

    feat: Cleanup the unnecessary files and update .gitignore files for secrets..

---
+++ lib/core/config/agora_config.dart
@@ -0,0 +1,6 @@
+class AgoraConfig {
+  static const String appId = "e8c3f657d2234bc1b1abef4874d5f39f";
+  // For production, you should use a token server to generate tokens
+  // For testing, you can use a temporary token from Agora Console or disable token
+  static const String token = "007eJxTYCg5JLTMYt7+SzI6XMeV1ed2KiQ82jLhweTfJwqvB5gW1IgpMKRaJBunmZmapxgZGZskJRsmGSYmpaaZWJibpJimGVumLdm8PbMhkJGhzSKMmZEBAkF8NgbHnNLcvEwGBgCbGiCV"; 
+}
