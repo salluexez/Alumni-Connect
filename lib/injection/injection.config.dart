340541c765bbcc8385163ab70511f70da8574bdd
Mohd Salauddin <sallumalik1111@gmail.com>
2026-04-02 21:34:12 +0530

    ci: add mock firebase configuration and enable build_runner generation in CI workflow while removing unused injection config file

--- lib/injection/injection.config.dart
+++ lib/injection/injection.config.dart
@@ -1,4 +1,5 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
+// dart format width=80
 
 // **************************************************************************
 // InjectableConfigGenerator
@@ -12,18 +13,12 @@ import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
 
 extension GetItInjectableX on _i174.GetIt {
-// initializes the registration of main-scope dependencies inside of GetIt
-  Future<_i174.GetIt> init({
+  // initializes the registration of main-scope dependencies inside of GetIt
+  _i174.GetIt init({
     String? environment,
     _i526.EnvironmentFilter? environmentFilter,
-  }) async {
-    // ignore: unused_local_variable
-    final gh = _i526.GetItHelper(
-      this,
-      environment,
-      environmentFilter,
-    );
-    // TODO: Register dependencies here as you add features
+  }) {
+    _i526.GetItHelper(this, environment, environmentFilter);
     return this;
   }
 }
